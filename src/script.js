ed395080e68499019aa5db819071112bb34048a3
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-07-17 13:01:04 -0300

    Add create book button to display form

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ function addBookToLibrary(book) {
 function displayBooks() {
   for (let book of myLibrary) {
     const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
+    bookCard.classList.add('bookCard');
     const title = document.createElement('h3');
     title.innerText = book.title;
     const author = document.createElement('p');
@@ -35,6 +35,37 @@ function displayBooks() {
     const booksDiv = document.querySelector('#books');
     booksDiv.appendChild(bookCard);
   }
+}
+
+const addNewBookBtn = document.getElementById('addNewBookBtn');
+const formContainer = document.getElementById('bookFormContainer');
+
+addNewBookBtn.addEventListener('click', () => {
+  createForm();
+});
+
+
+function createForm() {
+  const form = document.createElement('form');
+  form.innerHTML = `
+    <label for="title">Title:</label>
+    <input type="text" id="title" name="title">
+
+    <label for="author">Author:</label>
+    <input type="text" id="author" name="author">
+
+    <label for="numPages">Number of pages:</label>
+    <input type="number" id="numPages" name="numPages">
+
+    <div class="horizontal">
+      <label for="hasBeenRead">Has been read?</label>
+      <input type="checkbox" name="hasBeenRead" id="hasBeenRead">   
+    </div>
+
+    <label for="year">Publishing year:</label>
+    <input type="number" name="year" id="year">
+  `
+  formContainer.appendChild(form);
 }
 
 window.onload = displayBooks();
